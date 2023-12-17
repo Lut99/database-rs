bf0aeeac00600d2207aa8092c231810b63fd1ce3
Lut99 <mullertim99@gmail.com>
2023-12-17 20:51:08 +0100

    Going to create an SQLite implementation next.

---
+++ src/sqlite.rs
@@ -0,0 +1,16 @@
+//  SQLITE.rs
+//    by Lut99
+//
+//  Created:
+//    17 Dec 2023, 20:50:18
+//  Last edited:
+//    17 Dec 2023, 20:50:23
+//  Auto updated?
+//    Yes
+//
+//  Description:
+//!   Implements [`Database`] for an SQLite backend.
+//
+
+
+/***** LIBRARY *****/
